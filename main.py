3fc5b271c6d995373e2aaa386b7930fd67bfc833
Piyush Mehta <me@piyushmehta.com>
2019-09-26 23:28:58 +0530

    speak

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ import speech_recognition as sr
 from gtts import gTTS
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def speak(text):
+    tts = gTTS(text=text, lang="en")
+    filename = "voice.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+
+
+speak("I'm Gabbar.Py")
